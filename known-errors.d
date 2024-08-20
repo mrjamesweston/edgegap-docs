90df7e798330683e771fe9d7a5c67fbae8413413
Austin <austingaudet68@gmail.com>
2024-08-20 07:12:54 -0500

    Create known-errors.d

---
+++ known-errors.d
@@ -0,0 +1,7 @@
+# This is a basic list of issues you may encounter with very simple solutions.
+  ? > Ran into something you couldn't find in this page? [DM AUSTIN ON TWITTER](https://twitter.com/austinjgaudet) or on Discord at austin.james.
+
+  > Can't verify my Edgegap Token
+  When you're entering your [Edgegap Token that you retrieved from your Dashboard](https://app.edgegap.com/user-settings?tab=tokens), ensure the token that you are getting the One-Click Token. This is denoted by it stating that under the Quick Start category by saying Active.
+
+  ![edgegap dashboard](https://i.imgur.com/KCiU7z0.png)
